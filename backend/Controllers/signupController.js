aca883fb1e435851640622f42270696b57cafd58
Anmol sharma <as7067647@gmail.com>
2025-05-08 13:14:43 +0530

    update something

--- backend/Controllers/signupController.js
+++ backend/Controllers/signupController.js
@@ -4,14 +4,11 @@ const User = require("../models/signup");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-
 exports.signupUser = async (req, res) => {
-
   try {
     const { Name, Email, Contact_Number, Password } = req.body;
     const userExists = await User.findOne({ Email });
-    if (userExists)
-      return res.status(400).json({ msg: "User already exists" });
+    if (userExists) return res.status(400).json({ msg: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(Password, 6);
     const newUser = await User.create({
@@ -22,9 +19,9 @@ exports.signupUser = async (req, res) => {
     });
 
     const token = jwt.sign({ id: newUser._id }, JWT_SECRET, {
-      expiresIn: "1h",
+      expiresIn: "1d",
     });
-    console.log("JWT_SECRET:", JWT_SECRET)
+    console.log("JWT_SECRET:", JWT_SECRET);
 
     res.status(201).json({ token, user: { id: newUser._id, Email } });
   } catch (error) {
