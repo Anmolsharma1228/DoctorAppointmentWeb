851baba69d72f1ec24c7365c19b9e76a01ca42bb
Anmol sharma <as7067647@gmail.com>
2025-06-21 14:05:20 +0530

    add getuploadfile

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ dotenv.config();
 const app = express();
 dbConnection();
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
